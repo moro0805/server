c3a793b4825fafbc0c1f3d2a3c7fae32a438d2b1
moro <hiroyukihaikyu0805@gmail.com>
2024-03-21 13:40:56 +0900

    サーバプログラム更新

--- serialtest.py
+++ serialtest.py
@@ -1,6 +1,18 @@
-import serial
+import serial 
 
-ser = serial.Serial('/dev/ttyACM0.',9600,timeout=None)
-line = ser.readline()
-print(line) 
-ser.close()
+# シリアル通信を行うためのオブジェクトを作成 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10) 
+
+# 最初に受信するデータは値がおかしいので無視 
+is_first = True 
+
+while True: 
+
+# シリアル通信で受け取ったデータをUTF-8にデコードし,表示する 
+    lux = ser.readline().decode('utf-8') 
+    if is_first: 
+        is_first = False 
+    else: 
+        print(lux) 
+
+ser.close() 
